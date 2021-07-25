28b64cdcd38ac062098e7421a53c359859327798
mukul262610 <mukul@stargate-inc.comgit config --global user.email mukul@stargate-inc.com>
2021-07-25 08:04:23 +0530

    make it better3

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const connectDB = require('./config/db')
-const cors = require('cors')
+const cors = require('cors');
+const path = require('path');
 const app = express();
+const config = require('config')
+
+const node_env = config.get('node_env')
 
 app.use(cors())
 
 const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-    res.send("Hello World")
-})
 
 // Connect DB
 connectDB()
@@ -23,9 +23,21 @@ app.use("/api/blogs", require('./routes/blogs'))
 app.use("/api/auth", require('./routes/auth'))
 app.use("/api/profile", require('./routes/profile'))
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static("client/build"))
+
+
+if (node_env === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/build')))
+
+    console.log('Wortks')
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send("Hello World")
+    })
 }
+
 
 app.listen(PORT, () => {
     console.log(`Server is starting on port ${PORT}`)
